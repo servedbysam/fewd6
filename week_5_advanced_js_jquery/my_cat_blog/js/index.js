2f0cab8edda356a004ad205a4889475304aa80d7
servedbysam <Sam Peters>
2014-09-17 16:54:15 -0400

    Created array
    Created an array to auto populate blogposts. However, I cannot yet get
    them to appear on the website. I will continue to work on this.

--- week_5_advanced_js_jquery/my_cat_blog/js/index.js
+++ week_5_advanced_js_jquery/my_cat_blog/js/index.js
@@ -9,11 +9,24 @@ var posts = [
 		snippet: 'Wow...thats all I can say. I was surfing /cats on reddit and came across the cutest cat pictures yet! Take a look!'
 	}, {
 		image: 'http://placepu.gs/200/200',
-		title: 'Wierd cats!',
+		title: 'Weird cats!',
 		snippet: 'I saw these weird looking cats on the interwebs today. If you can tell me what breed they are, please do so in the comments!'
 	}, {
 		image: 'http://placekitten.com/g/200/200',
-		title: 'Cutness overload!',
+		title: 'Cuteness overload!',
 		snippet: 'OMG people, are you ready for some heavy duty cuteness? I know I am. Check out these cats! I can\'t even handle this. Too cute!'
 	}
 ];
+
+function generateBlogPosts(posts) {
+	var ret = '<h3>title</h3>';
+	ret += '<p>';
+	ret += 'snippet';
+	ret += 'image';
+	}
+
+for (var i = 0; i < posts.length; i++) {
+	html += generateBlogPosts(posts[i]);
+	}
+
+document.getElementById('posts').innerHTML = posts;
